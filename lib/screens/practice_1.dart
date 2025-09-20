21f2b977859b84a5bff90493c85ccf3061295316
shokhrukhorolov <shoxs5058@icloud.com>
2025-09-20 23:43:27 +0500

    Weather App 1.1

---
+++ lib/screens/practice_1.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class practice_1 extends StatefulWidget {
+  const practice_1({super.key});
+
+  @override
+  State<practice_1> createState() => _practice_1State();
+}
+
+class _practice_1State extends State<practice_1> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
