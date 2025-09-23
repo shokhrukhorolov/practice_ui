e5b0ca678d96cf5f4646ca03bfe39f1f175e0fc0
shokhrukhorolov <shoxs5058@icloud.com>
2025-09-23 15:42:28 +0500

    Completed Fundametals repetition 23.09

---
+++ lib/practice_log/practice_2.dart
@@ -0,0 +1,51 @@
+// class Person {
+//   String name;
+//   int age;
+//   double height;
+//   int weight;
+//
+//   Person(this.name, this.age, this.height, this.weight);
+// }
+//
+// void main () {
+//   var show = Person("Shokhrukh", 2005, 170.99, 65);
+//   print(show.name);
+// }
+
+
+// class Animal {
+//   void makeSound() => print("Some sound");
+// }
+//
+//
+// class Dog extends Animal {
+//   @override
+//   void makeSound() => print("Woof!");
+//
+// }
+//
+//
+// void main () {
+//   var sound = Dog();
+//   sound.makeSound();
+// }
+
+
+// class Rectangle {
+//   int width;
+//   int height;
+//
+//   Rectangle(this.height, this.width);
+//
+//   int area() => width * height;
+// }
+//
+// void main() {
+//
+//   var rectangular = Rectangle(4, 5);
+//   var answer = rectangular.area();
+//   print(answer);
+//
+// }
+
+
