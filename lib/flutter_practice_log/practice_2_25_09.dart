a04cbbad6ead2860b0a904c856450668e4fa23de
shokhrukhorolov <shoxs5058@icloud.com>
2025-09-26 18:15:20 +0500

    first out of 3 extra exercises completed

--- lib/flutter_practice_log/practice_2_25_09.dart
+++ lib/flutter_practice_log/practice_2_25_09.dart
@@ -40,6 +40,25 @@ import 'package:flutter/material.dart';
 //     );
 //   }
 // }
+//
+// class DomThree extends StatelessWidget {
+//   const DomThree({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title:  Text('Screen SECONDU')),
+//       backgroundColor: Colors.white,
+//       body: Center(
+//         child: ElevatedButton(onPressed: () {
+//           Navigator.pop(context);
+//         },
+//           child: const Text('Go back'),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 // class User {
 //   var name;
@@ -183,35 +202,156 @@ import 'package:flutter/material.dart';
 // }
 
 
-class TaskListScreen extends StatefulWidget {
-  const TaskListScreen({super.key});
+// class TaskListScreen extends StatefulWidget {
+//   const TaskListScreen({super.key});
+//
+//   @override
+//   State<TaskListScreen> createState() => _TaskListScreenState();
+// }
+//
+// class _TaskListScreenState extends State<TaskListScreen> {
+//   List<String> tasks = [];
+//
+//   void addTask() {
+//     setState(() {
+//       tasks.add("Task ${tasks.length + 1}");
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text("Tasks")),
+//       body: ListView.builder(
+//           itemCount: tasks.length,
+//           itemBuilder: (context, index) {
+//             return ListTile(title: Text(tasks[index]),);},
+//       ),
+//       floatingActionButton: FloatingActionButton(onPressed: addTask, child: Icon(Icons.add),),
+//     );
+//   }
+// }
 
-  @override
-  State<TaskListScreen> createState() => _TaskListScreenState();
-}
+class NavigationBarApp extends StatelessWidget {
+  const NavigationBarApp({super.key});
 
-class _TaskListScreenState extends State<TaskListScreen> {
-  List<String> tasks = [];
-  
-  void addTask() {
-    setState(() {
-      tasks.add("Task ${tasks.length + 1}");
-    });
-  }
-  
   @override
   Widget build(BuildContext context) {
+    return const MaterialApp(home: NavigationExample());
+  }
+}
+
+class NavigationExample extends StatefulWidget {
+  const NavigationExample({super.key});
+
+  @override
+  State<NavigationExample> createState() => _NavigationExampleState();
+}
+
+class _NavigationExampleState extends State<NavigationExample> {
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
     return Scaffold(
-      appBar: AppBar(title: Text("Tasks")),
-      body: ListView.builder(
-          itemCount: tasks.length,
-          itemBuilder: (context, index) {
-            return ListTile(title: Text(tasks[index]),);},
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        indicatorColor: Colors.amber,
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Badge(child: Icon(Icons.notifications_sharp)),
+            label: 'Notifications',
+          ),
+          NavigationDestination(
+            icon: Badge(label: Text('2'), child: Icon(Icons.messenger_sharp)),
+            label: 'Messages',
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(onPressed: addTask, child: Icon(Icons.add),),
+      body: <Widget>[
+        /// Home page
+        Card(
+          shadowColor: Colors.transparent,
+          margin: const EdgeInsets.all(8.0),
+          child: SizedBox.expand(
+            child: Center(child: Text('Home page', style: theme.textTheme.titleLarge)),
+          ),
+        ),
+
+        /// Notifications page
+        const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.notifications_sharp),
+                  title: Text('Notification 1'),
+                  subtitle: Text('This is a notification'),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.notifications_sharp),
+                  title: Text('Notification 2'),
+                  subtitle: Text('This is a notification'),
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        /// Messages page
+        ListView.builder(
+          reverse: true,
+          itemCount: 2,
+          itemBuilder: (BuildContext context, int index) {
+            if (index == 0) {
+              return Align(
+                alignment: Alignment.centerRight,
+                child: Container(
+                  margin: const EdgeInsets.all(8.0),
+                  padding: const EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                    color: theme.colorScheme.primary,
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: Text(
+                    'Hello',
+                    style: theme.textTheme.bodyLarge!.copyWith(color: theme.colorScheme.onPrimary),
+                  ),
+                ),
+              );
+            }
+            return Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
+                margin: const EdgeInsets.all(8.0),
+                padding: const EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                  color: theme.colorScheme.primary,
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Text(
+                  'Hi!',
+                  style: theme.textTheme.bodyLarge!.copyWith(color: theme.colorScheme.onPrimary),
+                ),
+              ),
+            );
+          },
+        ),
+      ][currentPageIndex],
     );
   }
 }
-
-
-
