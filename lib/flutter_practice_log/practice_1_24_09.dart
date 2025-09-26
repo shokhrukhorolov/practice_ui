420b69b23ce17764158a17c9bb9d756570164a02
shokhrukhorolov <shoxs5058@icloud.com>
2025-09-26 14:57:08 +0500

    finished half of tasks from 09/25

--- lib/flutter_practice_log/practice_1_24_09.dart
+++ lib/flutter_practice_log/practice_1_24_09.dart
@@ -1,36 +1,36 @@
-import 'package:flutter/material.dart';
-
-class DayOne extends StatefulWidget {
-  const DayOne({super.key});
-
-  @override
-  State<DayOne> createState() => _DayOneState();
-}
-
-class _DayOneState extends State<DayOne> {
-  int count = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('App')),
-      backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Center(child: Text('Hello, World!')),
-          const SizedBox(height: 20),
-          Text('Count: $count'),
-          TextButton(
-            onPressed: () {
-              setState(() {
-                count++;
-              });
-            },
-            child: const Text('Increment'),
-          ),
-        ],
-      ),
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+//
+// class DayOne extends StatefulWidget {
+//   const DayOne({super.key});
+//
+//   @override
+//   State<DayOne> createState() => _DayOneState();
+// }
+//
+// class _DayOneState extends State<DayOne> {
+//   int count = 0;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: const Text('App')),
+//       backgroundColor: Colors.white,
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: [
+//           const Center(child: Text('Hello, World!')),
+//           const SizedBox(height: 20),
+//           Text('Count: $count'),
+//           TextButton(
+//             onPressed: () {
+//               setState(() {
+//                 count++;
+//               });
+//             },
+//             child: const Text('Increment'),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
--- lib/flutter_practice_log/practice_2_25_09.dart
+++ lib/flutter_practice_log/practice_2_25_09.dart
@@ -1,4 +1,26 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+
+// class DomOne extends StatelessWidget {
+//   const DomOne({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title:  Text('Screen One')),
+//       backgroundColor: Colors.white,
+//       body: Center(
+//         child: ElevatedButton(child: const Text('Open route'),
+//           onPressed: () {
+//             Navigator.push(context, MaterialPageRoute<void>(builder: (context) => const DomTwo(),)
+//             );
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
+//
+//
 //
 // class DomTwo extends StatelessWidget {
 //   const DomTwo({super.key});
@@ -8,6 +30,94 @@
 //     return Scaffold(
 //       appBar: AppBar(title:  Text('Screen SECONDU')),
 //       backgroundColor: Colors.white,
+//       body: Center(
+//         child: ElevatedButton(onPressed: () {
+//           Navigator.pop(context);
+//         },
+//         child: const Text('Go back'),
+//         ),
+//       ),
 //     );
 //   }
 // }
+
+// class User {
+//   var name;
+//
+//   User(this.name);
+//
+//   void givename() {
+//     print('hello my name is ${name}');
+//   }
+// }
+//
+//
+// void main() {
+//   var name = User('Guest');
+//   name.givename();
+// }
+
+
+// abstract class Shape {
+//   double area();
+// }
+//
+// class Rectangle extends Shape {
+//   double width;
+//   double height;
+//
+//   Rectangle(this.width, this.height);
+//
+//   @override
+//   double area() {
+//     return width * height;
+//   }
+// }
+//
+// class Circle extends Shape {
+//   double radius;
+//
+//   Circle(this.radius);
+//
+//   @override
+//   double area() {
+//     return 3.14 * radius * radius;
+//   }
+// }
+//
+// void main() {
+//   Shape rect = Rectangle(10, 5);
+//   Shape circle = Circle(4);
+//
+//   print("rectangle's area: ${rect.area()}");
+//   print("circle's ares: ${circle.area()}");
+// }
+
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int counter = 0;
+
+  void increment() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Counter'),),
+      body: Center(child: Text("Counter: $counter",style: TextStyle(fontSize: 28),),),
+      floatingActionButton: FloatingActionButton(onPressed: increment,
+        child: Icon(Icons.add),),
+    );
+  }
+}
+
