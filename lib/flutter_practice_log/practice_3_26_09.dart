4e55c2cb56d3cb2431cfc0ffab721ff71cc74e0c
shokhrukhorolov <shoxs5058@icloud.com>
2025-09-29 17:15:20 +0500

    26.09 separeted lists, long lists, grid, horizontal lists done

--- lib/flutter_practice_log/practice_3_26_09.dart
+++ lib/flutter_practice_log/practice_3_26_09.dart
@@ -49,3 +49,44 @@ import 'package:flutter/material.dart';
 // }
 
 
+// class SeparatedList extends StatelessWidget {
+//   const SeparatedList({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     const items = 4;
+//
+//     return Scaffold(
+//       body: LayoutBuilder(
+//         builder: (context, constraints) {
+//           return SingleChildScrollView(
+//             child: ConstrainedBox(
+//               constraints: BoxConstraints(minHeight: constraints.maxHeight),
+//               child: Column(
+//                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                 crossAxisAlignment: CrossAxisAlignment.stretch,
+//                 children: List.generate(
+//                   items,
+//                       (index) => ItemWidget(text: 'Item $index'),
+//                 ),
+//               ),
+//             ),
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
+//
+// class ItemWidget extends StatelessWidget {
+//   const ItemWidget({super.key, required this.text});
+//
+//   final String text;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Card(
+//       child: SizedBox(height: 100, child: Center(child: Text(text),),),
+//     );
+//   }
+// }
