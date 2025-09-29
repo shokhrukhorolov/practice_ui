e2924ee052e432b1ad65663a4a6bbe4f3205824d
shokhrukhorolov <shoxs5058@icloud.com>
2025-09-29 19:17:25 +0500

    26.09 separeted lists, long lists, grid, horizontal lists done + extra exercises done + git file updated

--- lib/leetcode/LT_easy.dart
+++ lib/leetcode/LT_easy.dart
@@ -1,2 +1,19 @@
-// Two Sums
-
+// // Two Sums
+//
+// class Solution {
+//   List<int> twoSum(List<int> nums, int target) {
+//     Map<int, int> seen = {};
+//
+//     for (int i = 0; i < nums.length; i++) {
+//       int complement = target - nums[i];
+//
+//       if (seen.containsKey(complement)) {
+//         return [seen[complement]!, i];
+//       }
+//
+//       seen[nums[i]] = i;
+//     }
+//
+//     return [];
+//   }
+// }
