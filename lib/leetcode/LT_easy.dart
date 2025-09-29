c4b0b5a84e9fa1647113192ebf9d5ed3fef2a6a5
shokhrukhorolov <shoxs5058@icloud.com>
2025-09-29 19:23:47 +0500

    26.09 separeted lists, long lists, grid, horizontal lists done + extra exercises done + git file updated second version

--- lib/leetcode/LT_easy.dart
+++ lib/leetcode/LT_easy.dart
@@ -17,3 +17,20 @@
 //     return [];
 //   }
 // }
+
+
+// another version but it seems to be slower
+
+// class Solution {
+//   List<int> twoSum(List<int> nums, int target) {
+//
+//     for (int i = 0; i < nums.length; i++) {
+//       for (int j = i + 1; j < nums.length; j++ ) {
+//         if (nums[i] + nums[j] == target) {
+//           return [i, j];
+//         }
+//       }
+//     }
+//     return [];
+//   }
+// }
