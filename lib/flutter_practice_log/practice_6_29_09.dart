611b6492326fffdce617574c9ca3634c32905e4a
shokhrukhorolov <shoxs5058@icloud.com>
2025-10-03 16:15:25 +0500

    profile

--- lib/flutter_practice_log/practice_6_29_09.dart
+++ lib/flutter_practice_log/practice_6_29_09.dart
@@ -3,28 +3,13 @@ import 'package:flutter/material.dart';
 class Profile extends StatelessWidget {
   const Profile({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: Text('Profile')),
-//       body: Column(
-//       ),
-//     );
-//   }
-//   Widget box(String text) => Container(
-//     width: 80,
-//     height: 80,
-//     color: Colors.blue[200],
-//     child: Center(child: Text(text, style: TextStyle(fontSize: 24))),
-//   );
-// }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Profile'), centerTitle: true,),
       body: Padding( padding: EdgeInsets.all(16),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           CircleAvatar(
             radius: 50,
@@ -34,9 +19,12 @@ class Profile extends StatelessWidget {
 
           const SizedBox(height: 16),
 
-          Text(""
+          Text(textAlign: TextAlign.center,
               "Jack Daniels",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+              ),
           ),
 
           const SizedBox(height: 8),
@@ -47,30 +35,92 @@ class Profile extends StatelessWidget {
             style: TextStyle(fontSize: 16),
           ),
 
-          const SizedBox(height: 20),
+          const Divider(height: 60, color: Colors.black45, thickness: 4, ),
 
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              statBox("120", "Posts"),
-              statBox("2.5K", "Followers"),
-              statBox("300", "Following"),
+                Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'First name',
+                  ),
+                ),
+                ),
+
+              SizedBox(width: 16),
+
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Last name',
+                  ),
+                ),
+              ),
             ],
           ),
+
+          const SizedBox(height: 16),
+
+          Row(
+            children: [
+              Expanded(child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter your email'
+                ),
+              ))
+            ],
+          ),
+
+          const SizedBox(height: 16),
+
+          Row(
+            children: [
+              Expanded(child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter your phone number'
+                ),
+              ))
+            ],
+          ),
+
+          const SizedBox(height: 16),
+
+          Row(
+            children: [
+              Expanded(child: TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  hintText: 'Enter your date of birth'
+                ),
+              ))
+            ],
+          ),
+
+          Divider(height: 100 ,thickness: 4, color: Colors.black45,),
+
+          Row(
+            children: [
+              Expanded(child: ElevatedButton(onPressed: () {
+
+              },
+                child: const Text('Change'),
+              )),
+
+
+              SizedBox(width: 120),
+              Expanded(child: ElevatedButton(onPressed: () {
+
+              }, child: const Text('Save')
+              ))
+            ],
+          ),
+
+
+
+
           ],
-        )
-      ),
-    );
-  }
-  Widget statBox(String number, String label) {
-    return Column(
-      children: [
-        Text(
-          number,
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
         ),
-        Text(label, style: TextStyle(color: Colors.grey[600])),
-      ],
+      ),
     );
   }
 }
