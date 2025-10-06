ab027a7c17fcd00994b539c1d5945ee2c59be0d9
shokhrukhorolov <shoxs5058@icloud.com>
2025-10-06 15:28:04 +0500

    task 3  out of 5 + provider added

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:ui_practices/flutter_practice_log/practice_07_30_09.dart';
+import 'package:ui_practices/flutter_practice_log/practice_08_01_10.dart';
 import 'package:ui_practices/flutter_practice_log/practice_1_24_09.dart';
 import 'package:ui_practices/flutter_practice_log/practice_2_25_09.dart';
 import 'package:ui_practices/flutter_practice_log/practice_3_26_09.dart';
 import 'package:ui_practices/flutter_practice_log/practice_4_27_09.dart';
 import 'package:ui_practices/flutter_practice_log/practice_5_28_09.dart';
 import 'package:ui_practices/flutter_practice_log/practice_6_29_09.dart';
-import 'package:ui_practices/provider/counter_provider.dart';
+import 'package:ui_practices/models/counter_provider.dart';
+import 'package:ui_practices/models/navigation.dart';
 
 void main() {
   runApp(
@@ -21,12 +23,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CounterScreen(),
+      home: NavigatorExampleApp(),
     );
   }
 }
